40bea48cbb5862592424ca8a9110cc94b81a9c10
Meli Fetaji <melfetaji@gmail.com>
2025-05-15 10:35:05 +0200

    chore: security branch implementation

--- src/controllers/itemController.js
+++ src/controllers/itemController.js
@@ -1,19 +1,28 @@
-const itemService = require('../services/itemService');
+const itemService = require('../services/itemService')
 
 exports.createItem = async (req, res) => {
-  try {
-    const newItem = await itemService.createItem(req.body);
-    res.status(201).json(newItem);
-  } catch (error) {
-    res.status(500).json({ error: 'Internal server error' });
-  }
-};
+	try {
+		const newItem = await itemService.createItem(req.body)
+		res.status(201).json(newItem)
+	} catch (error) {
+		res.status(500).json({ error: 'Internal server error' })
+	}
+}
 
 exports.getAllItems = async (req, res) => {
-  try {
-    const items = await itemService.getAllItems();
-    res.json(items);
-  } catch (error) {
-    res.status(500).json({ error: 'Internal server error' });
-  }
-};
+	try {
+		const items = await itemService.getAllItems()
+		res.json(items)
+	} catch (error) {
+		res.status(500).json({ error: 'Internal server error' })
+	}
+}
+
+exports.searchItems = async (req, res) => {
+	try {
+		const results = await itemService.searchItems(req.query)
+		res.json(results)
+	} catch (err) {
+		res.status(500).json({ error: err.message, stack: err.stack })
+	}
+}
