5da7c5834da595150c9b2e900a662452b39c0d30
Meli Fetaji <melfetaji@gmail.com>
2025-05-15 10:07:17 +0200

    chore: performance branch implementation

--- server.js
+++ server.js
@@ -1,22 +1,28 @@
-const express = require('express');
-const helmet = require('helmet');
-const cors = require('cors');
+const express = require('express')
+const helmet = require('helmet')
+const cors = require('cors')
+const moment = require('moment')
 
-const { morganMiddleware } = require('./src/utils/logger');
-const itemRoutes = require('./src/routes/itemRoutes');
-const { PORT } = require('./src/config/env');
+const { morganMiddleware } = require('./src/utils/logger')
+const itemRoutes = require('./src/routes/itemRoutes')
+const { PORT } = require('./src/config/env')
 
-const app = express();
+const app = express()
 
 // Middlewares
-app.use(helmet());
-app.use(cors());
-app.use(express.json());
-app.use(morganMiddleware);
+app.use(helmet())
+app.use(cors())
+app.use(express.json())
+app.use(morganMiddleware)
+
+app.use((req, res, next) => {
+	req.requestTime = moment().format('YYYY-MM-DD HH:mm:ss')
+	next()
+})
 
 // Routes
-app.use('/api/items', itemRoutes);
+app.use('/api/items', itemRoutes)
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+	console.log(`Server running on port ${PORT}`)
+})
--- src/services/itemService.js
+++ src/services/itemService.js
@@ -7,12 +7,23 @@ module.exports = {
     return items;
   },
 
-  createItem: (itemData) => {
-    const newItem = {
-      id: Date.now(),
-      ...itemData
-    };
-    items.push(newItem);
-    return newItem;
-  },
-};
+	createItem: (itemData) => {
+		const newItem = {
+			id: Date.now(),
+			...itemData,
+		}
+		items.push(newItem)
+		return newItem
+	},
+
+	deleteItem: (id) => {
+		let removed = false
+		for (let i = 0; i < items.length; i++) {
+			if (items[i].id === parseInt(id, 10)) {
+				items.splice(i, 1)
+				removed = true
+			}
+		}
+		return removed
+	},
+}
